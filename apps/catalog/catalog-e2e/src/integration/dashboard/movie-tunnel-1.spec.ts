a69e7488511930c18e5aaeea733ed1579fd03485
Laurent Senta <laurent.senta@gmail.com>
2020-02-03 15:22:19 +0100

    movie tunnel wip

--- apps/catalog/catalog-e2e/src/integration/dashboard/movie-tunnel-1.spec.ts
+++ apps/catalog/catalog-e2e/src/integration/dashboard/movie-tunnel-1.spec.ts
@@ -1,10 +1,9 @@
 /// <reference types="cypress" />
-
 import { StartTunnelPage, TitlesListPage, TunnelMainPage } from '../../support/pages/dashboard';
-import { HomePage } from '../../support/pages/marketplace';
 import { User } from '../../support/utils/type';
 import { USERS } from '../../support/utils/users';
 import { LoginViewPage, WelcomeViewPage } from '../../support/pages/auth';
+import { HomePage } from '../../support/pages/marketplace';
 
 // TEST
 
@@ -18,18 +17,22 @@ beforeEach(() => {
   cy.viewport('ipad-2', 'landscape');
 });
 
-describe('User can navigate to the movie tunnel page 1 and 2', () => {
-  it('Login into an existing account, navigate on titles list page, go to movie tunnel page 1, go on movie tunnel page 2', () => {
-    // https://github.com/blockframes/blockframes/issues/1494
+describe('issue#1494: movie tunnel page 1', () => {
+  // https://github.com/blockframes/blockframes/issues/1494
 
-    // Connexion
+  it('Login into an existing account, navigate on titles list page, go to movie tunnel page 1, go on movie tunnel page 2', () => {
+    // The user connects,
     const p1: WelcomeViewPage = new WelcomeViewPage();
     const p2: LoginViewPage = p1.clickCallToAction();
     p2.switchMode();
     p2.fillSignin(LOGIN_CREDENTIALS);
     const p3: HomePage = p2.clickSignIn();
 
-    // Navigate to movie-tunnel-1
+    // CAN'T BE DONE:
+    //   Lands on homepage = dashboard page,
+    //   Clicks on the add button,
+    // INSTEAD:
+    //   Navigate to movie-tunnel-1
     const p4: TitlesListPage = TitlesListPage.navigateToPage();
     const p5: StartTunnelPage = p4.clickAdd();
     const p6: TunnelMainPage = p5.clickBegin();
